4b1411f014a6dc259093c38df35812f1736d0bfb
heetrue <bonnylovesyou@naver.com>
2022-08-22 22:32:57 +0900

    :art: style: Add custom width class (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        68: '17.1875rem',
+      },
+    },
   },
   variants: {
     extend: {},
