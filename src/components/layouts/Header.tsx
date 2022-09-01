77e39e8bb2d473cec965346e60d051171377a460
heetrue <bonnylovesyou@naver.com>
2022-09-02 01:11:23 +0900

    :sparkles: feat: Add user account menu in header (#7)

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,15 +1,34 @@
-import Logo from 'assets/logo.svg';
 import Navigation from './Navigation';
+
+import Logo from 'assets/logo.svg';
+import MoreIcon from 'assets/icons/icon-more.svg';
 
 const Header = () => {
   return (
-    <header className="flex-col w-68 h-full px-3">
-      <h1 className="py-0.5">
-        <a href="/" className="flex content-center w-fit h-13 rounded-full">
-          <Logo width="50" height="30" />
-        </a>
-      </h1>
-      <Navigation />
+    <header className="flex flex-col justify-between justify-self-end min-w-[275px] h-screen px-3 overflow-y-auto">
+      <div>
+        <h1 className="py-0.5">
+          <a href="/" className="flex content-center w-fit h-13 rounded-full">
+            <Logo width="50" height="30" />
+          </a>
+        </h1>
+        <Navigation />
+      </div>
+      <button
+        type="button"
+        className="flex items-center w-full p-3 my-3 rounded-full text-base hover:bg-[#E7E7E7] transition duration-100 hover:ease-in"
+      >
+        <div className="w-10 h-10 rounded-full bg-slate-100">
+          {/* <img src="#" alt="" /> */}
+        </div>
+        <div className="flex flex-col mx-3 leading-snug text-left">
+          <strong className="font-bold">희진</strong>
+          <p className="text-[#536471]">@heejinna</p>
+        </div>
+        <div className="flex justify-end items-center flex-grow">
+          <MoreIcon width="19" height="19" fill="#0f1419" />
+        </div>
+      </button>
     </header>
   );
 };
